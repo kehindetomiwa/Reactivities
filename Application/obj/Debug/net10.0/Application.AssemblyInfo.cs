0f150282b4b53e7dd56251f8a3b7581e4e81e6da
Kehinde Tomiwa <kehinde.tomiwa-cloud2@outlook.com>
2026-07-16 17:10:13 +0200

    dotnet backend API

--- Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbcc8dfd9c33eda69a9201ef9d72debb64630e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8aed8136c2d169029cd1d27c7a0376d7f4198da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
